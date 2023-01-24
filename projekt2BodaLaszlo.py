ca9235a2c8c8914893ac77394e87e9d61d316f25
lacus1 <113436003+lacus1@users.noreply.github.com>
2023-01-24 23:47:48 +0100

    Add files via upload

---
+++ projekt2BodaLaszlo.py
@@ -0,0 +1,8 @@
+num1 = float(input("Adj meg egy számot: "))
+num2 = float(input("Adj meg egy másik számot: "))
+
+if num1 > num2:
+    print(num1, "a nagyobb szám")
+else:
+    print(num2, "a nagyobb szám")
+print("Program vége")
